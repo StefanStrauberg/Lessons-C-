655791d5913f3a620a55e78290c665d67aab6030
Strauberg <StefanStrauberg@gmail.com>
2024-03-11 17:02:15 +0300

    small fix2

--- 06_ArithmeticalOperations/priorityOfOperations.cpp
+++ 06_ArithmeticalOperations/priorityOfOperations.cpp
@@ -4,10 +4,12 @@ int main()
 {
     int a = 8;
     int b = 7;
-    int result = a + 5 * ++b;
     std::cout << "a is: " << a << std::endl;
     std::cout << "b is: " << b << std::endl;
+    
+    int result = a + 5 * ++b;
+    
     std::cout << "Priority: increment, decriment, mul, div, remainder, sum, diff" << std::endl;
-    std::cout << "a + 8 * ++b = " << result << std::endl;
+    std::cout << "a + 5 * ++b = " << result << std::endl;
     return 0;
 }
