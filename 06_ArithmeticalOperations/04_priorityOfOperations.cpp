2d122ae81c6be273d23f6e048c15692cce3105ee
Strauberg <StefanStrauberg@gmail.com>
2024-03-12 13:58:12 +0300

    complite with postfix and prefix

--- 06_ArithmeticalOperations/04_priorityOfOperations.cpp
+++ 06_ArithmeticalOperations/04_priorityOfOperations.cpp
@@ -12,14 +12,34 @@ int main()
     std::cout << "Priority: increment, decriment, mul, div, remainder, sum, diff" << std::endl;
     std::cout << "a + 5 * ++b = " << result << std::endl;
 
+    a = 8;
+    b = 7;
+    std::cout << "a is: " << a << std::endl;
+    std::cout << "b is: " << b << std::endl;
+
+    result = a + 5 * b++;
+
+    std::cout << "Priority: increment, decriment, mul, div, remainder, sum, diff" << std::endl;
+    std::cout << "a + 5 * b++ = " << result << std::endl;
+
     int c = 1;
     std::cout << "c is: " << c << std::endl;
     std::cout << "++c*2 + c++ = " << ++c*2 + c++ << std::endl;
     std::cout << "and then c is: " << c << std::endl;
 
-    int d = 1;
-    std::cout << "d is: " << d << std::endl;
-    std::cout << "d++*2 + d++ = " << d++*2 + ++d << std::endl;
-    std::cout << "and then d is: " << d << std::endl;
+    c = 1;
+    std::cout << "c is: " << c << std::endl;
+    std::cout << "c++*2 + c++ = " << c++*2 + c++ << std::endl;
+    std::cout << "and then c is: " << c << std::endl;
+
+    c = 1;
+    std::cout << "c is: " << c << std::endl;
+    std::cout << "c++*2 + ++c = " << c++*2 + ++c << std::endl;
+    std::cout << "and then c is: " << c << std::endl;
+
+    c = 1;
+    std::cout << "c is: " << c << std::endl;
+    std::cout << "++c*2 + ++c = " << ++c*2 + ++c << std::endl;
+    std::cout << "and then c is: " << c << std::endl;
     return 0;
 }
